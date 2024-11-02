7df0bd7d9de7724d0387fed534995dfeb940a38d
Aleksandra <asandrabelka@gmail,com>
2024-11-02 17:58:29 +0200

    Add Seminar4 Task 1 & 2

---
+++ Lecture3Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Output squares of numbers from 1 to N.
+
+void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+Console.WriteLine();
+PrintSquares(10);
+Console.WriteLine();
+PrintSquares(15);
